38c8a60aa30c14c67c6a14fe420836e88ac974ff
Ismael Oliveira <ismael6991@live.com>
2023-04-05 10:47:19 -0300

    Injetando templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {
+  ModalRef,
+  ModalService,
+} from './shared/components/modal/services/modal.service';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef!: TemplateRef<any>;
   title = 'design-acessivel-angular-2';
+  firstName: string = 'Ismael';
+  modalRef!: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
 }
